62a6d4f8c76f8e765b1eb8307429f375a4930d11
heiwa4126 <heiwa4126@gmail.com>
2024-04-18 15:12:30 +0900

    update?

---
+++ examples/ex1.js
@@ -0,0 +1,5 @@
+const { fizzbuzz } = require("@heiwa4126/fizzbuzz");
+
+for (const i of fizzbuzz(15)) {
+	console.log(i);
+}
---
+++ examples/ex2.js
@@ -0,0 +1,3 @@
+const { fizzbuzzAt } = require("@heiwa4126/fizzbuzz");
+
+console.log(fizzbuzzAt(15));
